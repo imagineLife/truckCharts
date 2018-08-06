896d1ab17153b28527bd042602ba85250b96f19f
Eric Laursen <ericjlaursen@gmail.com>
2018-08-06 14:44:21 -0400

    Includes 2nd bar chart.

---
+++ src/components/TrucksPerHourChart/index.js
@@ -0,0 +1,141 @@
+import React, { Component } from 'react'
+import { scaleBand, scaleLinear } from 'd3-scale'
+
+import data from '../../data'
+import yesterdayData from '../../dataYesterday'
+import AxisLabel from '../AxisLabel'
+import AxesAndMath from '../Axes'
+import Bars from '../Bars'
+import Line from '../Line'
+import ResponsiveWrapper from '../ResponsiveWrapper'
+import './style.css';
+
+class TrucksPerHourChart extends Component {
+  constructor() {
+    super()
+    this.xScale = scaleBand().padding(0.2)
+    this.yScale = scaleLinear()
+    this.calcXPos = this.calcXPos.bind(this)
+    this.state = {
+      labels: [
+        { 
+          type:'x',
+          text : 'Time Of Day',
+          textClass : 'xAxisLabelText',
+          gWrapperClass : 'xAxisLabelG',
+          transformation: ''
+        }, 
+        {
+          type: 'y',
+          text : 'Number Of Trucks',
+          textClass : 'yAxisLabelText',
+          gWrapperClass : 'yAxisLabelG',
+          transformation: 'rotate(-90)'
+        },
+        {
+          type: 'chartTitle',
+          text : 'Trucks Per Hour In Facility',
+          textClass : 'chartTitle',
+          gWrapperClass : 'chartTitleG',
+          transformation: ''
+        },
+
+      ],
+      margins : { top: 75, right: 20, bottom: 100, left: 60 }
+    }
+  }
+
+  calcXPos(string, dims){
+    if(string.indexOf('y') > -1){
+      return -(dims.height / 2)
+    }else if(string.indexOf('c') > -1){
+      return (dims.width / 2)
+    }else{
+      return ( dims.width / 2)
+    }
+
+  }
+
+  calcYPos(string, dims){
+    if(string.indexOf('y') > -1){
+      return dims.width * .02
+    }else if(string.indexOf('c') > -1){
+      return (dims.height * .05)
+    }else{
+      return dims.height - 25
+    }
+
+  }
+
+  render() {
+    
+    //set svg dimensions
+    const svgDimensions = {
+      width: Math.max(this.props.parentWidth, 300),
+      height: 550
+    }
+
+    //max value from data
+    const maxDataValue = Math.max(...data.map(d => d.truckCount))
+
+    const xScale = this.xScale
+      .domain(data.map(d => d.hour))
+      .range([this.state.margins.left, svgDimensions.width - this.state.margins.right])
+
+    const yScale = this.yScale
+      .domain([0, maxDataValue])
+      .range([svgDimensions.height - this.state.margins.bottom, this.state.margins.top])
+
+    const axisLabels = this.state.labels.map((each) => {
+
+      return <AxisLabel
+        key={each.text}
+        xPos={this.calcXPos(each.type, svgDimensions)}
+        yPos={this.calcYPos(each.type, svgDimensions)}
+        labelClass={each.textClass}
+        groupClass={each.gWrapperClass}
+        textVal={each.text}
+        transformation={each.transformation}
+      />
+    })
+    //return
+    //SVG
+    //  AxesAndMath Component
+    //  BARS component
+    return (
+      <svg 
+        className="trucksPerHourSVG"
+        width={svgDimensions.width}
+        height={svgDimensions.height}
+        style={{'margin-bottom': '75px'}} >
+
+        <AxesAndMath
+          scales={{ xScale, yScale }}
+          margins={this.state.margins}
+          svgDimensions={svgDimensions}
+        />
+        
+        <Bars
+          scales={{ xScale, yScale }}
+          margins={this.state.margins}
+          data={data}
+          maxValue={maxDataValue}
+          svgDimensions={svgDimensions}
+        />
+
+        <Line
+          scales={{ xScale, yScale }}
+          margins={this.state.margins}
+          data={yesterdayData}
+          maxValue={maxDataValue}
+          svgDimensions={svgDimensions}
+        /> 
+
+        {axisLabels}
+
+      </svg>
+    )
+  }
+}
+
+export default ResponsiveWrapper(TrucksPerHourChart)
