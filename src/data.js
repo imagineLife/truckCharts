e36df5e446d1e34b81b2415955039ef46ead13f3
Eric Laursen <ericjlaursen@gmail.com>
2018-08-06 18:07:17 -0400

    Updates data.

--- src/data.js
+++ src/data.js
@@ -1,37 +1,93 @@
 export default [
   {
     "hour": '9:00',
-    "truckCount": 12,
-    "commodity": "YC"
+    "trucks":[
+      {
+        "truckCount": 8,
+        "commodity": "YC"
+      },
+      {
+        "truckCount": 4,
+        "commodity": "SB"
+      }
+    ]
   },
   {
     "hour": '10:00',
-    "truckCount": 13,
-    "commodity": "YC"
+    "trucks":[
+      {
+        "truckCount": 8,
+        "commodity": "YC"
+      },
+      {
+        "truckCount": 5,
+        "commodity": "SB"
+      }
+    ]
   },
   {
     "hour": '11:00',
-    "truckCount": 14,
-    "commodity": "SB"
+    "trucks":[
+      {
+        "truckCount": 10,
+        "commodity": "YC"
+      },
+      {
+        "truckCount": 4,
+        "commodity": "SB"
+      }
+    ]
   },
   {
     "hour": '1:00',
-    "truckCount": 15,
-    "commodity": "YC"
+    "trucks":[
+      {
+        "truckCount": 12,
+        "commodity": "YC"
+      },
+      {
+        "truckCount": 4,
+        "commodity": "SB"
+      }
+    ]
   },  
   {
     "hour": '2:00',
-    "truckCount": 14,
-    "commodity": "SB"
+    "trucks":[
+      {
+        "truckCount": 12,
+        "commodity": "YC"
+      },
+      {
+        "truckCount": 6,
+        "commodity": "SB"
+      }
+    ]
   },
   {
     "hour": '3:00',
-    "truckCount": 13,
-    "commodity": "YC"
+    "trucks":[
+      {
+        "truckCount": 8,
+        "commodity": "YC"
+      },
+      {
+        "truckCount": 2,
+        "commodity": "SB"
+      }
+    ]
   },
   {
     "hour": '4:00',
-    "truckCount": 12,
-    "commodity": "SB"
+    "trucks":[
+      {
+        "truckCount": 7,
+        "commodity": "YC"
+      },
+      {
+        "truckCount": 4,
+        "commodity": "SB"
+      }
+    ]
   }
 ]
