3461712ec273f267537c79bde56ac0f0a06e0669
Eric Laursen <ericjlaursen@gmail.com>
2018-08-07 16:31:53 -0400

    Wraps app in provider.

--- src/index.js
+++ src/index.js
@@ -3,8 +3,12 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import './index.css';
 import './float-grid.css';
+import { Provider } from 'react-redux';
+import chartStore from './store';
 
 ReactDOM.render(
-  <App />,
+	<Provider store={chartStore}>
+  		<App />
+  	</Provider>,
   document.getElementById('root'),
 );
