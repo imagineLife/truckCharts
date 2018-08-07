a12d8862d334f52cbcbba5ab88c7922f79ee9229
Eric Laursen <ericjlaursen@gmail.com>
2018-08-07 16:32:26 -0400

    Sets redux store from form vals.

--- src/components/Settings/index.js
+++ src/components/Settings/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import './settings.css'
+import { Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-export default class Settings extends React.Component {
+export class Settings extends React.Component {
 
   constructor(props){
     super(props)
 
     this.state = {
-      tphLimit: '',
-      mpfLimit: ''
+      alerts: {
+        tphLimit: '',
+        mpfLimit: ''
+      },
+      submittedForm:false
     }
     
     this.submitForm = this.submitForm.bind(this)
@@ -18,52 +23,63 @@ export default class Settings extends React.Component {
 
   updateFormVal(e){
     let idVal = e.target.id
-    let curState = this.state
-    curState[idVal] = e.target.value
-    this.setState(curState)
+    let curAlerts = this.state.alerts
+    curAlerts[idVal] = e.target.value
+    this.setState({alerts: curAlerts})
 
   }
 
   submitForm(e){
     e.preventDefault();
-    console.log('submitted form')
-    console.log('this.state')
-    console.log(this.state)
+    this.props.dispatch({type: 'updateAlertLevels', payload: this.state.alerts})
+    this.setState({submittedForm: true})
+
   }
 
   render(){
 
-  	return (
-  	<div className="settingsWrapper">
-      <div className="headerSpacer"></div>
-      <h2> Charts Alert-Levels Settings</h2>
-        <form className="settingsForm">
-          
-          <label className="settingsLabel" htmlFor="tphLimit">Minimum Trucks Per Hour Alert:
-            <input 
-              className="settingsInput" 
-              id="tphLimit" 
-              name="tphLimit" 
-              value={this.state.tphLimit} 
-              onChange={this.updateFormVal}
-            />
-          </label>
+    if(this.state.submittedForm){
+      return(
+        <Redirect to='/charts' />
+      )
+    }else{
+      return (
+        <div className="settingsWrapper">
+          <div className="headerSpacer"></div>
+          <h2> Charts Alert-Levels Settings</h2>
+            <form className="settingsForm">
+              
+              <label className="settingsLabel" htmlFor="tphLimit">Minimum Trucks Per Hour Alert:
+                <input 
+                  className="settingsInput" 
+                  id="tphLimit" 
+                  name="tphLimit" 
+                  value={this.state.alerts.tphLimit} 
+                  onChange={this.updateFormVal}
+                />
+              </label>
 
-          <label className="settingsLabel" htmlFor="mpfLimit">Maximum Facility Minitues Alert:
-            <input 
-                className="settingsInput" 
-                id="mpfLimit" 
-                name="mpfLimit" 
-                value={this.state.mpfLimit}
-                onChange={this.updateFormVal}
-              />
-          </label>
+              <label className="settingsLabel" htmlFor="mpfLimit">Maximum Facility Minitues Alert:
+                <input 
+                    className="settingsInput" 
+                    id="mpfLimit" 
+                    name="mpfLimit" 
+                    value={this.state.alerts.mpfLimit}
+                    onChange={this.updateFormVal}
+                  />
+              </label>
 
-          <input type="button" name="submit" id='submit' onClick={this.submitForm} value="Set Alert Levels" />
-        
-        </form>		
-		</div>
-  	);
+              <input type="button" name="submit" id='submit' onClick={this.submitForm} value="Set Alert Levels" />
+            
+            </form>   
+        </div>
+      );
+    }
+  	
   
   }
 }
+
+const mapStateToProps = state => ({storeAlerts: state})
+
+export default connect(mapStateToProps)(Settings);
