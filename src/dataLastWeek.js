4d2df4f4c107321eb9f109835cbf2de774db7a54
Eric Laursen <ericjlaursen@gmail.com>
2018-08-08 10:42:08 -0400

    placeholder for more line data.

---
+++ src/dataLastWeek.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    "hour": '9:00',
+    "truckCount": 8,
+    "commodity": "YC"
+  },
+  {
+    "hour": '10:00',
+    "truckCount": 9,
+    "commodity": "YC"
+  },
+  {
+    "hour": '11:00',
+    "truckCount": 10,
+    "commodity": "SB"
+  },
+  {
+    "hour": '1:00',
+    "truckCount": 10,
+    "commodity": "YC"
+  },  
+  {
+    "hour": '2:00',
+    "truckCount": 11,
+    "commodity": "SB"
+  },
+  {
+    "hour": '3:00',
+    "truckCount": 12,
+    "commodity": "YC"
+  },
+  {
+    "hour": '4:00',
+    "truckCount": 10,
+    "commodity": "SB"
+  }
+]
