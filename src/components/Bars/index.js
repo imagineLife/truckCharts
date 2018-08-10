7f9c6f8fe1cb92c6dadf9941d6ac8b8754c9bf2a
Eric Laursen <ericjlaursen@gmail.com>
2018-08-10 08:51:55 -0400

    Converts to functional component, updates accordingly.

--- src/components/Bars/index.js
+++ src/components/Bars/index.js
@@ -1,46 +1,28 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { scaleOrdinal } from 'd3-scale'
 
-export default class Bars extends Component {
-  constructor(props) {
-    super(props)
+export default function Bars(props) {
 
-    this.colorScale = scaleOrdinal()
+    let colorScale = scaleOrdinal()
       .range(['steelblue', 'green']);
 
-    this.showBarDetails = this.showBarDetails.bind(this)
-
-    this.state = {}
-  }
-
-  showBarDetails(barData){
-    console.log('showing bar details')
-    console.log(barData)
-  }
-
-  componentDidMount(){
-    if (!this.state.barData){
-      this.setState({barData: this.props.data});
-    }
-  }
-
-  render() {
-    const { scales, margins, data, svgDimensions } = this.props
+    const { scales, margins, data, svgDimensions } = props
     const { xScale, yScale } = scales
     const { height } = svgDimensions
 
     const bars = (
       data.map(barData => {
-          if(barData.hour){
+        if(barData.hour){
 
-            let thisTruckData = barData.trucks;
-            let totalTruckCountThisHour = 0;
-            thisTruckData.map((t) => {
-              return totalTruckCountThisHour += t.truckCount;
-            })
+          let thisTruckData = barData.trucks;
+          let totalTruckCountThisHour = 0;
+          thisTruckData.map((t) => {
+            return totalTruckCountThisHour += t.truckCount;
+          })
 
 
-            return <rect
+          return (
+            <rect
               key={barData.hour}
               x={xScale(barData.hour) + (xScale.bandwidth() * .1)}
               y={yScale(totalTruckCountThisHour)}
@@ -48,29 +30,32 @@ export default class Bars extends Component {
               width={xScale.bandwidth() * .75}
               fill={'cadetblue'}
               stroke={'gray'}
-              onClick={() => this.showBarDetails(barData)}
+              onClick={() => props.showBarDetails(barData)}
+              onMouseOver={() => props.mousedOver(barData)}
               // fill={'rgba(255,255,255,.5)'}
             />
-            
-          }else{
-            return <rect
+          )
+        }else{
+          return ( 
+            <rect
               key={barData.truckID}
               x={xScale(barData.truckID)}
               y={yScale(barData.minutes)}
               height={height - margins.bottom - scales.yScale(barData.minutes)}
               width={xScale.bandwidth()}
-              fill={this.colorScale(barData.commodity)}
-              onClick={() => this.showBarDetails(barData)}
+              fill={colorScale(barData.commodity)}
+              onClick={() => props.showBarDetails(barData)}
+              onMouseOver={() => props.mousedOver(barData)}
               stroke={'gray'}
               // fill={'rgba(255,255,255,.5)'}
             />
-          }
+          )
         }
-      )
+
+      })
     )
 
     return (
       <g>{bars}</g>
     )
-  }
 }
