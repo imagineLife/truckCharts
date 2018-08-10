b1cb54f4ef3e836aacdfd11fc2665edad39deb54
Eric Laursen <ericjlaursen@gmail.com>
2018-08-10 08:51:17 -0400

    Simplifies alert logic.

--- src/components/Alert/index.js
+++ src/components/Alert/index.js
@@ -1,27 +1,25 @@
 import React from 'react';
 // import {Link} from 'react-router-dom';
 import './alert.css';
+import alertImageImport from '../../imgs/alert.ico'
+import okImgImport from '../../imgs/ok.png'
+
 import { connect } from 'react-redux';
 
 export function Alert(props) {
 
 	let reduxAlertStatus = props.reduxAlerts.containerAlertStatus;
-	console.log('alert component props.reduxAlerts.containerAlertStatus')
-	console.log(reduxAlertStatus)
-	console.log('- - - -')
-	if(reduxAlertStatus){
-		return(
-			<div>
-				<p>TRUE Alert Component</p>
-			</div>
-		);	
-	}else{
-		return(
-			<div>
-				<p>FALSE Alert Component</p>
-			</div>
-		);
-	}
+
+	let srcStr = (reduxAlertStatus) ? alertImageImport : okImgImport;
+	let altTxt = (reduxAlertStatus) ? 'alert!' : 'running smoothly';
+	let viewTxt = (reduxAlertStatus) ? 'Alerts' : 'Running Smoothly';
+
+	return(
+		<div>
+			<img className="bigAlertIcon" src={srcStr} alt={altTxt}/>
+			<h2 className="bigAlertText">{viewTxt}</h2>
+		</div>
+	);	
 	
 }
 
