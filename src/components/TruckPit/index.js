4e155670cd6b9f1a9f5535a30ae01e21979730c4
Eric Laursen <ericjlaursen@gmail.com>
2018-08-13 15:02:18 -0400

    Conditionally renders 2 charts.

--- src/components/TruckPit/index.js
+++ src/components/TruckPit/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import TruckPitInChart from '../TruckPitInChart'
 import TruckPitOutChart from '../TruckPitOutChart'
+import cornerClicker from '../../imgs/cornerClicker.png'
 
 
 // import data from '../../minutesPerTruck '
@@ -18,22 +19,49 @@ class TruckPit extends Component {
       margins : { top: 75, right: 20, bottom: 100, left: 60 },
     }
 
+    this.updateViewNumber = this.updateViewNumber.bind(this)
+
   }
 
 
-  render() {
+  componentDidMount(){
+    console.log('CDM viewNumber')
+  }
 
-    console.log('viewNumber')
+  updateViewNumber(){
+    console.log('CLICKED')
+    let newNumber = (this.state.viewNumber ===1) ? 2 : 1;
+    this.setState({viewNumber: newNumber})
+  }
+
+  render() {
+    console.log('RENDERING view number')
     console.log(this.state.viewNumber)
 
-    return (
-      <div className="truckPit">
-        <h2>Trucks Today:</h2>
-        <TruckPitInChart />
-        <TruckPitOutChart />
-      </div>
-
-    )
+    if(this.state.viewNumber ===1){
+      return(
+        <div className="truckPit">
+          <div className="cornerClicker">
+            <img src={cornerClicker} onClick={this.updateViewNumber}/>
+          </div>
+          <h2>Trucks Today:</h2>
+          <TruckPitInChart />
+          <TruckPitOutChart />
+        </div>
+      )
+    }
+    if(this.state.viewNumber ===2){
+      return(
+        <div className="truckPit total">
+          <div className="cornerClicker">
+            <img src={cornerClicker} onClick={this.updateViewNumber}/>
+          </div>          
+          <h2>Trucks Today By Pit:</h2>
+          <TruckPitInChart />
+          <TruckPitOutChart />
+        </div>
+      )
+    }
   }
 }
 
